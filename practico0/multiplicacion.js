ec85e8a6842d9209fb7c97d8c18734b89c7d6feb
laubg <laurabautistg@gmail.com>
2023-05-28 04:47:33 -0300

    resta y multiplicacion.js ya agregados a carpeta

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,9 @@
+// 1) b. Dado tres números calcule la multiplicación entre ellos y lo imprima por
+// pantalla. Deberá guardar este programa en un archivo llamado:
+// multiplicacion.js. Ayuda: utilizar console.log.
+// ● ¿Qué valor devuelve su programa si los números son 2 *17 * 6? R:204
+
+let n1= 2;
+let n2= 17;
+let n3=6;
+console.log(n1*n2*n3);
---
+++ practico0/resta.js
@@ -0,0 +1,10 @@
+// 1) a. Dado dos números calcule la resta entre ambos; donde el primer número
+// es el minuendo y el segundo es el sustraendo. Imprima el resultado por
+// pantalla.
+// ¿Qué valor devuelve su programa si el minuendo es 10 y el
+// sustraendo 2? R:8
+
+let n1= 10;
+let n2= 2;
+let resta= n1-n2;
+console.log(resta);
